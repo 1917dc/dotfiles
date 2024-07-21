0711eee4f9d6eb7938d6c732b32c9cd68a9d66bf
Luiz G <l19051997@gmail.com>
2024-07-21 14:29:50 -0300

    criado o script de instalação do thinkpad

---
+++ Thinkpad/thinkpad_installation_script.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+sudo apt install sway swaybg mako-notifier pulseaudio pavucontrol waybar fonts-font-awesome kitty tmux
+
+sudo apt install build-essential
+sudo apt install git
+sudo apt install python3-pip
+
+pip3 install --user meson==0.55.3pip3 install --user meson==0.55.3
+
+export PATH=$HOME/.local/bin:$PATH
+
+sudo apt install wayland-protocols \
+libwayland-dev \
+libegl1-mesa-dev \
+libgles2-mesa-dev \
+libdrm-dev \
+libgbm-dev \
+libinput-dev \
+libxkbcommon-dev \
+libgudev-1.0-dev \
+libpixman-1-dev \
+libsystemd-dev \
+cmake \
+libpng-dev \
+libavutil-dev \
+libavcodec-dev \
+libavformat-dev \
+ninja-build \
+meson
+
+sudo apt install libxcb-composite0-dev \
+        libxcb-icccm4-dev \
+        libxcb-image0-dev \
+        libxcb-render0-dev \
+        libxcb-xfixes0-dev \
+        libxkbcommon-dev \
+        libxcb-xinput-dev \
+        libx11-xcb-dev
+
+sudo apt install libjson-c-dev \
+libpango1.0-dev \
+libcairo2-dev \
+libgdk-pixbuf2.0-dev \
+scdoc
+
+sudo apt install wl-clipboard
+sudo apt install grim
+sudo apt install slurp
+sudo apt install jq
+sudo apt install grimshot
+ 
+sudo apt install brightnessctl
+
+
