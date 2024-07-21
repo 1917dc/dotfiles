61b97670542997227e46e2d26f5180a1fc4ad8ad
Luiz Gustavo <142899763+1917dc@users.noreply.github.com>
2024-07-20 21:14:27 -0300

    Criando script de instalação de configuração

---
+++ Desktop/script.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+echo ** SCRIPT FOR MY DESKTOP DOTFILES ** 
+echo Installing dependencies.
+
+sudo apt install git sway swaybg mako-notifier fzf pulseaudio wl-clipboard grim grimshot slurp jq pavucontrol waybar fonts-font-awesome kitty tmux 
+
+echo Dependencies installed.
+
+echo Now installing node, zsh, oh my zsh.
+
+echo installing zsh
+sudo apt install zsh
+chsh -s $(which zsh)
+
+echo now installing oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo installing neovim for nvchad
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
+
+export PATH="$PATH:/opt/nvim-linux64/bin"
+
+sudo apt install curl
+
+\n
+echo installing nvm for node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+\n
+echo installing node recent version
+nvm install node
+\n
+
+\n
+echo cloning my dotfiles to respective folders.
+\n
+
+cd && cd dotfiles/Desktop/
+
+cp -r kitty ~/.config/
+cp -r mako ~/.config/
+cp -r nvim ~/.config/
+cp -r sway ~/.config/
+cp -r waybar ~/.config/
+cp -r fastfetch ~/.config/
+
+cd && cp .tmux.conf ~/
+cd && cp .zshrc ~/
+cd && cp .fonts ~/
+
+sudo add-apt-repository ppa:zhangsongcui3371/fastfetch
+sudo apt update
+
+sudo apt install fastfetch
+
+echo \n\n ** SCRIPT COMPLETED INSTALLATION * \n\n
