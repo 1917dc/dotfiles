ce5a3e67953f7846a88ddb7bf396fea75557eeb6
Luiz Gustavo <142899763+1917dc@users.noreply.github.com>
2024-07-20 21:19:33 -0300

    Criando script de instalação de configuração

--- Desktop/script.sh
+++ Desktop/script.sh
@@ -17,6 +17,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 echo installing neovim for nvchad
 
+cd
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
